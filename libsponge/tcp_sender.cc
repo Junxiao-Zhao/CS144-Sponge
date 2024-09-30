706929f7f4f6f60c942ccb43d0527e6c7c13b26d
Junxiao Zhao <zhao_junxiao@hotmail.com>
2024-09-29 18:25:26 -0700

    finish lab3

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -1,6 +1,7 @@
 #include "tcp_sender.hh"
 
 #include "tcp_config.hh"
+#include "timer.hh"
 
 #include <random>
 
@@ -10,7 +11,7 @@
 // automated checks run by `make check_lab3`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -22,17 +23,71 @@ TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const s
     , _initial_retransmission_timeout{retx_timeout}
     , _stream(capacity) {}
 
-uint64_t TCPSender::bytes_in_flight() const { return {}; }
+uint64_t TCPSender::bytes_in_flight() const { return this->_next_seqno - this->_timer.num_acked(); }
 
-void TCPSender::fill_window() {}
+void TCPSender::fill_window() {
+    uint16_t window_size = max(this->_window_size, static_cast<uint16_t>(1));
+
+    while (window_size > bytes_in_flight()) {
+        TCPSegment seg;
+
+        // CLOSED -> SYN_SENT
+        if (!this->_syn_sent) {
+            seg.header().syn = true;
+            this->_syn_sent = true;
+        }
+
+        uint16_t payload_size =
+            min(TCPConfig::MAX_PAYLOAD_SIZE,
+                min(window_size - bytes_in_flight() - seg.header().syn, this->_stream.buffer_size()));
+        auto payload_content = this->_stream.read(payload_size);
+        seg.payload() = Buffer(move(payload_content));
+
+        // FIN_SENT
+        if (!this->_fin_sent && this->_stream.eof() &&
+            window_size > bytes_in_flight() + seg.length_in_sequence_space()) {
+            seg.header().fin = true;
+            this->_fin_sent = true;
+        }
+
+        uint64_t length = seg.length_in_sequence_space();
+
+        // Throw away empty segments
+        if (length == 0) {
+            break;
+        }
+
+        seg.header().seqno = next_seqno();
+        this->_segments_out.push(seg);
+        this->_next_seqno += length;
+        this->_timer.track(seg);
+    }
+}
 
 //! \param ackno The remote receiver's ackno (acknowledgment number)
 //! \param window_size The remote receiver's advertised window size
-void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) { DUMMY_CODE(ackno, window_size); }
+void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) {
+    this->_window_size = window_size;
+    this->_timer.remove_received(ackno);
+    this->fill_window();
+}
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void TCPSender::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPSender::tick(const size_t ms_since_last_tick) {
+    // Not yet timeout OR no outstanding segments
+    if (!this->_timer.resend(ms_since_last_tick)) {
+        return;
+    }
 
-unsigned int TCPSender::consecutive_retransmissions() const { return {}; }
+    this->_segments_out.push(this->_timer.outstandings().front().second);
+    if (this->_window_size > 0) {
+        this->_timer.extend_rto();
+    }
+}
 
-void TCPSender::send_empty_segment() {}
+unsigned int TCPSender::consecutive_retransmissions() const { return this->_timer.num_retransmissions(); }
+
+void TCPSender::send_empty_segment() {
+    TCPSegment seg;
+    this->_segments_out.push(seg);
+}
---
+++ libsponge/timer.hh
@@ -0,0 +1,66 @@
+#ifndef SPONGE_LIBSPONGE_TIMER_HH
+#define SPONGE_LIBSPONGE_TIMER_HH
+
+#include "tcp_config.hh"
+#include "tcp_segment.hh"
+#include "wrapping_integers.hh"
+
+#include <queue>
+
+using namespace std;
+
+//! \brief A timer that keeps track of the retransmission timeout.
+class Timer {
+  private:
+    WrappingInt32 _isn;                                 //! our initial sequence number, the number for our SYN.
+    bool _running{false};                               //! Flag indicating that the timer is running.
+    unsigned int _initial_retransmission_timeout;       //! The initial retransmission timeout.
+    unsigned int _rto;                                  //! The retransmission timeout.
+    unsigned int _num_retransmissions{0};               //! The number of consecutive retransmissions.
+    unsigned int _time_elapsed{0};                      //! The time elapsed since the last retransmission.
+    queue<pair<uint64_t, TCPSegment>> _outstandings{};  //! The outstanding segments.
+    uint64_t _nx_seqno{0};                              //! The (absolute) sequence number for the byte to be sent next.
+    uint64_t _num_acked{0};                             //! The number of bytes acknowledged.
+
+  public:
+    //! Initialize a Timer.
+    Timer(const unsigned int rto, WrappingInt32 isn) : _isn(isn), _initial_retransmission_timeout(rto), _rto(rto) {};
+
+    //! \name Methods that can cause the Timer to retransmit a segment
+    //!@{
+
+    //! \brief Track a segment.
+    void track(TCPSegment &seg);
+
+    //! \brief Remove segments that have been acknowledged.
+    uint64_t remove_received(const WrappingInt32 ackno);
+
+    //! \brief Indicate whether a segment should be resent.
+    bool resend(const size_t ms_since_last_tick);
+
+    //! \brief Extend the retransmission timeout.
+    void extend_rto() {
+        _num_retransmissions++;
+        _rto *= 2;
+    };
+    //!@}
+
+    //! \name Accessors
+    //!@{
+    bool running() const { return _running; }
+
+    unsigned int rto() const { return _rto; }
+
+    unsigned int num_retransmissions() const { return _num_retransmissions; }
+
+    unsigned int time_elapsed() const { return _time_elapsed; }
+
+    uint64_t next_seqno() const { return _nx_seqno; }
+
+    queue<pair<uint64_t, TCPSegment>> outstandings() const { return _outstandings; }
+
+    uint64_t num_acked() const { return _num_acked; }
+    //!@}
+};
+
+#endif  // SPONGE_LIBSPONGE_TIMER_HH
