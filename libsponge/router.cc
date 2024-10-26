2542d0ecf7de03ecf09c57944ec9426c4481d7f7
Junxiao Zhao <zhao_junxiao@hotmail.com>
2024-10-26 02:51:06 +0000

    finish lab6

--- libsponge/router.cc
+++ libsponge/router.cc
@@ -29,14 +29,39 @@ void Router::add_route(const uint32_t route_prefix,
     cerr << "DEBUG: adding route " << Address::from_ipv4_numeric(route_prefix).ip() << "/" << int(prefix_length)
          << " => " << (next_hop.has_value() ? next_hop->ip() : "(direct)") << " on interface " << interface_num << "\n";
 
-    DUMMY_CODE(route_prefix, prefix_length, next_hop, interface_num);
     // Your code here.
+    this->_routes.emplace_back(move(RouterRow{route_prefix, prefix_length, next_hop, interface_num}));
+}
+
+RouterRow* Router::longest_prefix_match(const uint32_t dest_ip) {
+    RouterRow *longest_match = nullptr;
+    size_t longest_match_length = 0;
+    size_t ip_prefix = this->MASK;
+    for (auto &router : this->_routes) {
+        ip_prefix = dest_ip & (this->MASK << (32 - router.prefix_length));
+
+        if (ip_prefix == router.route_prefix && router.prefix_length >= longest_match_length) {
+            longest_match = &router;
+            longest_match_length = router.prefix_length;
+        }
+    }
+
+    return longest_match;
 }
 
 //! \param[in] dgram The datagram to be routed
 void Router::route_one_datagram(InternetDatagram &dgram) {
-    DUMMY_CODE(dgram);
     // Your code here.
+    uint32_t dest_ip = dgram.header().dst;
+    RouterRow *longest_match = this->longest_prefix_match(dest_ip);
+
+    if (longest_match == nullptr || dgram.header().ttl <= 1) {
+        return;
+    }
+
+    dgram.header().ttl -= 1;
+    dest_ip = longest_match->next_hop.has_value() ? longest_match->next_hop->ipv4_numeric() : dest_ip;
+    this->_interfaces[longest_match->interface_num].send_datagram(dgram, Address::from_ipv4_numeric(dest_ip));
 }
 
 void Router::route() {
--- libsponge/router.hh
+++ libsponge/router.hh
@@ -38,6 +38,14 @@ class AsyncNetworkInterface : public NetworkInterface {
     std::queue<InternetDatagram> &datagrams_out() { return _datagrams_out; }
 };
 
+
+struct RouterRow {
+    uint32_t route_prefix;
+    uint8_t prefix_length;
+    std::optional<Address> next_hop;
+    size_t interface_num;
+};
+
 //! \brief A router that has multiple network interfaces and
 //! performs longest-prefix-match routing between them.
 class Router {
@@ -48,6 +56,12 @@ class Router {
     //! as specified by the route with the longest prefix_length that matches the
     //! datagram's destination address.
     void route_one_datagram(InternetDatagram &dgram);
+
+    std::vector<RouterRow> _routes{};
+
+    RouterRow* longest_prefix_match(const uint32_t dest_ip);
+
+    static constexpr size_t MASK = 0xFFFFFFFF;
 
   public:
     //! Add an interface to the router
